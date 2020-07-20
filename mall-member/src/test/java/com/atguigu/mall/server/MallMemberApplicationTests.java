e7b67d80bda171d599236eefaa0e5526fcd6d23d
winsonwen <winsonwen4@gmail.com>
2020-07-20 16:53:34 -0400

    initial Structure

---
+++ mall-member/src/test/java/com/atguigu/mall/server/MallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.mall.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallMemberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
